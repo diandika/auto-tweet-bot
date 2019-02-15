0017bbda44a7da25f09e55c8d54a7781d7376650
diandika <mohammaddicky.andika@gmail.com>
2019-02-15 23:01:28 +0700

    add stream filter

--- server.js
+++ server.js
@@ -4,8 +4,26 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+console.log(T);
 
-setInterval(tweetStatus, 1000*60);
+var fs = require('fs');
+var obj
+fs.readFile('./tweet_file.json',preprocess);
+
+function preprocess(err,data){
+  if (err){
+    throw err;
+  }
+  obj = JSON.parse(data);
+  console.log(obj);
+}
+
+var id_acc = 1094789694280261600;
+
+var stream = T.stream('statuses/filter', {follow: id_acc});
+stream.on('tweet', function(err, data, response){
+  console.log(data);
+});
 
 function tweetStatus(){
   var randInt = Math.floor(Math.random()*1000);
